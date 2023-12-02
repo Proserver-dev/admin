8817126d044115ba5ad37ee4a99d53d2b2872526
Hubert Machała <hubert.machala@o2.pl>
2023-12-02 22:17:25 +0100

    Fix: change messageToAll to messageFromServer

--- src/constants/SocketEvents.js
+++ src/constants/SocketEvents.js
@@ -3,7 +3,7 @@ const SocketEvents = {
     EMIT_MESSAGE_TO_ONE_USER: 'messageToOneUser',
     EMIT_PRIVATE_MESSAGE_CREATED_VIA_API: 'privateMessageCreatedViaAPI',
 
-    LISTEN_MESSAGE_FROM_SERVER: 'messageToAll', // TODO: później zmienić nazwę na messageFromServer
+    LISTEN_MESSAGE_FROM_SERVER: 'messageFromServer',
     LISTEN_NEW_SOCKET_CONNECTION: 'newSocketConnection',
     LISTEN_RESPONSE_FROM_SOCKET: 'responseFromSocket',
     LISTEN_CONNECTION_ACTION: 'connectionAction',
